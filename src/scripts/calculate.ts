71c54e976d09bcb18c22950cc710a12ec446991b
Cyrus <cyhfung@gmail.com>
2024-08-17 10:55:21 -0400

    finished

--- src/scripts/calculate.ts
+++ src/scripts/calculate.ts
@@ -1,9 +1,10 @@
 const calculate = (equation: string) => {
+  equation = equation.replace(/[\(\)]/g, "");
   let newEq: Array<string> = [""];
 
   // Get array of floats and operations
   for (let i = 0; i < equation.length; i++) {
-    if (equation[i].match(/[^\+\-\*\/\^\(\)]/g)) {
+    if (equation[i].match(/[^\+\-\*\/\^]/g)) {
       newEq = newEq
         .slice(0, -1)
         .concat(newEq[newEq.length - 1].concat(equation[i]));
@@ -14,18 +15,21 @@ const calculate = (equation: string) => {
 
   console.log(newEq);
 
-  const result = [];
-  let reserve = "";
+  const result: Array<Number> = [];
+  const reserve = [];
   for (let i = 0; i < newEq.length; i++) {
     const num = parseFloat(newEq[i]);
-    if (!isNaN(num)) {
+    if (isNaN(num)) {
+      reserve.push(newEq[i]);
+    } else {
       result.push(num);
       if (result.length === 2) {
         const b = result.pop();
         const a = result.pop();
+        const sym = reserve.pop();
         console.log(result);
-        console.log(a, reserve, b);
-        switch (reserve) {
+        console.log(a, sym, b);
+        switch (sym) {
           case "+":
             result.push(a + b);
             break;
@@ -45,8 +49,6 @@ const calculate = (equation: string) => {
             break;
         }
       }
-    } else {
-      reserve = newEq[i];
     }
   }
 
